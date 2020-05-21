2b7381ffacc96f52242bf173ee83fa33cca4690b
Radek Dvořák <radek.dvorak@gmail.com>
2020-05-22 00:33:05 +0200

    Severity conversion test

--- src/app/logging.rs
+++ src/app/logging.rs
@@ -20,3 +20,17 @@ fn verbosity_to_severity(verbosity: u8) -> Severity {
         4..=std::u8::MAX => Severity::Trace,
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    pub fn common_severities_converted() {
+        assert_eq!(Severity::Error, verbosity_to_severity(0));
+        assert_eq!(Severity::Warning, verbosity_to_severity(1));
+        assert_eq!(Severity::Info, verbosity_to_severity(2));
+        assert_eq!(Severity::Debug, verbosity_to_severity(3));
+        assert_eq!(Severity::Trace, verbosity_to_severity(4));
+    }
+}
